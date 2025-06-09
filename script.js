eb2206745a0da63543ac9674eb2633f0fe6e1639
enweremo <onyekachienwerem2@gmail.com>
2025-06-08 19:00:31 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -403,7 +403,9 @@ function updateCharts() {
   if (chart1 && typeof chart1.resize === "function") chart1.resize();
   if (chart2 && typeof chart2.resize === "function") chart2.resize();
   window.dispatchEvent(new Event('resize'));
-  if (chart1 && typeof chart1.update === "function") chart1.update();
+  setTimeout(() => {
+    if (chart1 && typeof chart1.update === "function") chart1.update();
+  }, 30)
 }
 
 
